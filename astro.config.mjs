253ad2aeebfec06af942503daea3ec87da5888ad
Meschac Irung <meschacirung@gmail.com>
2024-12-14 15:04:50 +0200

    remove static.yml workflow and update site URL in astro.config.mjs to point to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ciya.github.io',
-    base: "/ciya",
+    site: 'https://ciya.netlify.app',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown : {
         syntaxHighlight : "prism"
