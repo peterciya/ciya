6e216cffd5e0f32acdfc79e30a736541661abe4a
Meschac Irung <meschacirung@gmail.com>
2024-12-14 12:37:03 +0200

    refactor layout structure and enhance Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode : 'selector',
 	theme: {
-		extend: {},
+		extend: {
+			colors : ({colors}) => ({
+				gray : colors.zinc,
+			})
+		},
 	},
 	plugins: [],
 }
